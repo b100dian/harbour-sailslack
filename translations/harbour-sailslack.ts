3059aa5dbfd38ce22ea7e24bd7700a1d4312c091
Vlad G <b100dian@gmail.com>
2020-11-22 19:35:58 +0200

    updated ts file

--- translations/harbour-sailslack.ts
+++ translations/harbour-sailslack.ts
@@ -239,8 +239,18 @@ Are you sure you wish to leave?</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../qml/pages/MessageListView.qml" line="82"/>
+        <source>Thread in %1%2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
         <location filename="../qml/pages/MessageListView.qml" line="107"/>
         <source>Message %1%2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MessageListView.qml" line="107"/>
+        <source>Message thread in %1%2</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
